5ff94b6d8895ca1f5a32f7a4d9417350f246046b
Loise-Karuma <95070921+Loise-Karuma@users.noreply.github.com>
2022-10-29 21:48:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { routes } = require('./routes/index')
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require('./swagger.json')
+
 
 routes(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+var options = {
+  explorer: true
+};
+  
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+  
